d32b9f55ee46cec4ccb7fc529839ab08c645ab38
Ndeye Awa Diop <nadiop97@gmail.com>
2021-04-04 08:12:48 +0200

    Router set

---
+++ src/config/router.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Login from '../screens/login'
+import Home from '../screens/home'
+
+const Routes = () => {
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route path="/home" component={Home}/>
+        {<Redirect to='/'></Redirect>}
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
